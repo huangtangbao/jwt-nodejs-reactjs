e062be0ca36f324b621ed56a4315a5a3c18ecf78
huangtangbao <huangtangbao@gmail.com>
2025-04-09 22:21:12 +0700

    orm create

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2/promise'; // Sử dụng mysql2 với promise
 import bcrypt from 'bcryptjs';
+import db from '../models/index';
 // Tạo connection pool thay vì dùng createConnection
 const pool = mysql.createPool({
     host: 'localhost',
@@ -23,13 +24,19 @@ const hashedUserPassword = async (userhashPassword) => {
 const CreateNewUser = async (email, password, username) => {
     try {
         let hashPass = await hashedUserPassword(password); // Chờ hash hoàn thành
-        console.log("hashPassword:", hashPass);
+        /*console.log("hashPassword:", hashPass);
 
         const sql = 'INSERT INTO users (email, password, username) VALUES (?, ?, ?)';
         const [result] = await pool.execute(sql, [email, hashPass, username]);
 
         console.log("User đã được thêm:", result);
-        return result;
+        return result;*/
+        await db.User.create({
+            email: email,
+            username: username,
+            password: hashPass
+
+        });
     } catch (error) {
         console.error("Lỗi khi tạo user:", error);
         throw error;
